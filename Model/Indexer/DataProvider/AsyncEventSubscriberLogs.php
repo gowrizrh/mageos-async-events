95324ab15f621899a752b173195abfbfae5308f4
Gowri <gowrizrh@gmail.com>
2024-12-24 15:45:06 +1030

    :zap: reindex improvement for large counts

--- Model/Indexer/DataProvider/AsyncEventSubscriberLogs.php
+++ Model/Indexer/DataProvider/AsyncEventSubscriberLogs.php
@@ -4,17 +4,32 @@ declare(strict_types=1);
 
 namespace MageOS\AsyncEvents\Model\Indexer\DataProvider;
 
+use Generator;
+use Magento\Framework\App\DeploymentConfig;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use MageOS\AsyncEvents\Model\Indexer\AsyncEventSubscriber;
 use MageOS\AsyncEvents\Model\ResourceModel\AsyncEventLog\Collection;
 use MageOS\AsyncEvents\Model\ResourceModel\AsyncEventLog\CollectionFactory as AsyncEventLogCollectionFactory;
+use Zend_Db_Expr;
 
 class AsyncEventSubscriberLogs
 {
+    private AdapterInterface $connection;
+
+    private const DEPLOYMENT_CONFIG_INDEXER_BATCHES = 'indexer/batch_size/';
+
     /**
      * @param AsyncEventLogCollectionFactory $collectionFactory
+     * @param ResourceConnection $resource
+     * @param DeploymentConfig $deploymentConfig
      */
     public function __construct(
-        private readonly AsyncEventLogCollectionFactory $collectionFactory
+        private readonly AsyncEventLogCollectionFactory $collectionFactory,
+        private readonly ResourceConnection $resource,
+        private readonly DeploymentConfig $deploymentConfig
     ) {
+        $this->connection = $resource->getConnection();
     }
 
     /**
@@ -41,5 +56,36 @@ class AsyncEventSubscriberLogs
         }
 
         return $logCollection;
+    }
+
+    public function rebuildIndex(string $asyncEvent): Generator
+    {
+        $tableName = $this->resource->getTableName('async_event_subscriber_log');
+
+        $batchSize = $this->deploymentConfig->get(
+            self::DEPLOYMENT_CONFIG_INDEXER_BATCHES . AsyncEventSubscriber::INDEXER_ID . '/mysql_get'
+        ) ?? 10_000;
+
+        $lastId = 0;
+
+        while (true) {
+            $select = $this->connection->select()
+                ->from($tableName, ['*', new Zend_Db_Expr("'$asyncEvent' AS event_name")])
+                ->where('log_id > ?', $lastId)
+                ->order('log_id ASC')
+                ->limit($batchSize);
+
+            $result = $this->connection->fetchAll($select);
+
+            if (empty($result)) {
+                break;
+            }
+
+            foreach ($result as $row) {
+                yield $row['log_id'] => $row;
+            }
+
+            $lastId = end($result)["log_id"];
+        }
     }
 }
